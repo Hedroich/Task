2a85aa4d944d813a3d7ffe39f202a4bbf59636dc
Hedroich <hedroich@gmail.com>
2023-11-07 18:13:10 +0300

    Fix

--- task_user/forms.py
+++ task_user/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
